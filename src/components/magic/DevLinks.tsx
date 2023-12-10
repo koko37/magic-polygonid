4e30d4c5ad55b3d2c84d1d27f5c41b917064a51d
koko37 <engel0088@gmail.com>
2023-12-10 10:37:53 +0800

    add wallet/address modal

--- src/components/magic/DevLinks.tsx
+++ src/components/magic/DevLinks.tsx
@@ -1,26 +1,74 @@
-import classNames from 'classnames';
-import Link from 'next/link';
+import { useMagic } from "./MagicProvider";
+import classNames from "classnames";
+import Link from "next/link";
 
-const DevLinks = ({ primary = false, footer = false }: { primary?: boolean; footer?: boolean }) => (
-  <div className={`links ${footer ? 'footer-links' : ''}`}>
-    <div className={`link ${primary ? 'text-[#6851ff]' : 'text-[#fff]'}`}>
-      <a href="https://magic.link/docs/home/welcome" target="_blank" rel="noreferrer">
-        Dev Docs
-      </a>
+const DevLinks = ({
+  primary = false,
+  footer = false,
+}: {
+  primary?: boolean;
+  footer?: boolean;
+}) => {
+  const { magic } = useMagic();
+
+  const showWallet = async (e: React.MouseEvent<HTMLAnchorElement>) => {
+    // Your custom logic here
+    await magic?.wallet.showUI();
+  };
+  const showAddress = async (e: React.MouseEvent<HTMLAnchorElement>) => {
+    // Your custom logic here
+    await magic?.wallet.showAddress();
+  };
+
+  return (
+    <div className={`links ${footer ? "footer-links" : ""}`}>
+      <div className={`link ${primary ? "text-[#6851ff]" : "text-[#fff]"}`}>
+        <Link onClick={showWallet} href="#">
+          My Wallet
+        </Link>
+      </div>
+      <div
+        className={`link-divider ${primary ? "bg-[#DDDBE0]" : "bg-[#a270d3]"}`}
+      />
+      <div className={`link ${primary ? "text-[#6851ff]" : "text-[#fff]"}`}>
+        <Link onClick={showAddress} href="#">
+          My Address
+        </Link>
+      </div>
+      <div
+        className={`link-divider ${primary ? "bg-[#DDDBE0]" : "bg-[#a270d3]"}`}
+      />
+      <div className={`link ${primary ? "text-[#6851ff]" : "text-[#fff]"}`}>
+        <a
+          href="https://magic.link/docs/home/welcome"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Dev Docs
+        </a>
+      </div>
+      <div
+        className={`link-divider ${primary ? "bg-[#DDDBE0]" : "bg-[#a270d3]"}`}
+      />
+      <div className={`link ${primary ? "text-[#6851ff]" : "text-[#fff]"}`}>
+        <a
+          href="https://dashboard.magic.link/signup"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Dashboard
+        </a>
+      </div>
+      <div
+        className={`link-divider ${primary ? "bg-[#DDDBE0]" : "bg-[#a270d3]"}`}
+      />
+      <div className={`link ${primary ? "text-[#6851ff]" : "text-[#fff]"}`}>
+        <a href="https://discord.gg/magiclabs" target="_blank" rel="noreferrer">
+          Discord
+        </a>
+      </div>
     </div>
-    <div className={`link-divider ${primary ? 'bg-[#DDDBE0]' : 'bg-[#a270d3]'}`} />
-    <div className={`link ${primary ? 'text-[#6851ff]' : 'text-[#fff]'}`}>
-      <a href="https://dashboard.magic.link/signup" target="_blank" rel="noreferrer">
-        Dashboard
-      </a>
-    </div>
-    <div className={`link-divider ${primary ? 'bg-[#DDDBE0]' : 'bg-[#a270d3]'}`} />
-    <div className={`link ${primary ? 'text-[#6851ff]' : 'text-[#fff]'}`}>
-      <a href="https://discord.gg/magiclabs" target="_blank" rel="noreferrer">
-        Discord
-      </a>
-    </div>
-  </div>
-);
+  );
+};
 
 export default DevLinks;
